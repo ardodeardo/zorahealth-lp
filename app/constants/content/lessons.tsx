6a5fc9698c7901ce54da8b218192a3f4aabae9d6
ardodeardo <simadanaga@gmail.com>
2025-01-26 18:07:03 +0700

    chore: more content

--- app/constants/content/lessons.tsx
+++ app/constants/content/lessons.tsx
@@ -31,10 +31,45 @@ const lessons: Array<Lesson> = [
   },
   {
     uuid: "e888119a-2e9a-4f30-bbb1-6cbf8eabd2b2",
-    slug: "the-steps-and-decisions-in-the-ivf-process",
+    slug: "how-well-iui-works-by-patient-type",
     basepath: "lessons",
     subtitle: 'iui or "artificial insemination"',
     title: "How Well IUI Works by Patient Type",
+  },
+  {
+    uuid: "7bada154-ad43-49ef-8229-7261732160fb",
+    slug: "natural-vs-medical-vs-surgical-miscarriage-management",
+    basepath: "lessons",
+    subtitle: "misscariage",
+    title: "Natural vs. Medical vs. Surgical Miscarriage Management?",
+  },
+  {
+    uuid: "d1c77407-f67d-44a1-aa93-537b590ec426",
+    slug: "does-egg-freexing-work",
+    basepath: "lessons",
+    subtitle: "egg freezing",
+    title: "Does Egg Freexing Work?",
+  },
+  {
+    uuid: "b3cef20b-625d-4ef4-8e7d-c9ce3c58a814",
+    slug: "what-to-say-and-not-say",
+    basepath: "lessons",
+    subtitle: "wokrplce guide to fertility: for colleagues",
+    title: "What to Say & Not Say",
+  },
+  {
+    uuid: "78e1996f-07ff-478c-b4ef-a6416ae21375",
+    slug: "treatment-and-work-logistics",
+    basepath: "lessons",
+    subtitle: "managing fertility treatment work",
+    title: "Treatment & Work logistics",
+  },
+  {
+    uuid: "c867425c-968a-44ee-b4c6-248c215163e3",
+    slug: "the-domestic-adoption-process",
+    basepath: "lessons",
+    subtitle: "adoption",
+    title: "The Domestic Adoption Process",
   },
 ];
 
